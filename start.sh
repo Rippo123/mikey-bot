d5104d20e703603a5372d4426d32f807e394d21c
Rippo123 <98217663+Rippo123@users.noreply.github.com>
2022-05-26 13:59:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/200920082007/Master.git /EvaMaria
+  git clone https://github.com/Rippo123/mikey-bot.git /mikey-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /mikey-bot
 fi
-cd /EvaMaria
+cd /mikey-bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
